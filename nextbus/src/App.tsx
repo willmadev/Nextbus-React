8af281dceb06f13804465f99eba5535b483f829b
Willma <65773478+willmas-here@users.noreply.github.com>
2021-06-07 00:04:32 +0800

    Company Selector

--- nextbus/src/App.tsx
+++ nextbus/src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { FC, useState } from "react"
+
 import "./app.css"
-function App() {
+
+interface CompanySelectorProps {
+  value: string,
+  onChange: React.ChangeEventHandler<HTMLSelectElement>
+}
+
+const CompanySelector:FC<CompanySelectorProps> = ({value, onChange}) => {
+  return (
+    <div className="app_company-selector">
+      <label htmlFor="company">Select Company: </label>
+      <select name="company" value={value} onChange={onChange}>
+        <option value="">Select an option</option>
+        <option value="CTB">Citybus</option>
+        <option value="NWFB">New World First Bus</option>
+        <option value="NLB">New Lantao Bus Company</option>
+      </select>
+    </div>)
+}
+
+const App:FC = () => {
+  const [company, setCompany] = useState("")
+  const companySelected = (company !== "")
+  
+
   return (
     <div className="page-wrapper">
       <div className="header">
         <h1>Nextbus HK</h1>
-        <h3>Made by <a>Willma</a></h3>
+        <h3>Made by Willma</h3>
+      </div>
+      <div className="app_container">
+        <CompanySelector value={company} onChange={(e) => setCompany(e.target.value)}/>
+        <div className="app_route-selector">
+          <input type="search"></input>
+        </div>
       </div>
     </div>
   );
