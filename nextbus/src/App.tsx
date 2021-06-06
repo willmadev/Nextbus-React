ce09b9708b81c2fa37918e2bc3984842aa687da3
Willma <65773478+willmas-here@users.noreply.github.com>
2021-06-06 23:32:25 +0800

    Initial Commit

---
+++ nextbus/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./app.css"
+function App() {
+  return (
+    <div className="page-wrapper">
+      <div className="header">
+        <h1>Nextbus HK</h1>
+        <h3>Made by <a>Willma</a></h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
